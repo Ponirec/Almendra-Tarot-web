8db73b523f6df1e3bcbfe63baa8ef47eb7748ee9
Diego Ruiz <ponirecords@gmail.com>
2025-04-01 21:21:06 -0300

    Actualizacion menu hamburguesa

--- menuactivo.js
+++ menuactivo.js
@@ -46,9 +46,12 @@ document.addEventListener("DOMContentLoaded", () => {
     const toggle = document.getElementById("menu-toggle");
     const menu = document.getElementById("menu");
   
-    toggle.addEventListener("click", () => {
-      menu.classList.toggle("active");
-    });
+    if (toggle && menu) {
+      toggle.addEventListener("click", () => {
+        menu.classList.toggle("active");
+      });
+    }
   });
+  
   
   
